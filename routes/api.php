4e7e05080511445075f589dc69892c907f95bcba
Thilina Ilangarathna <thillangarathna@gmail.com>
2021-04-16 20:33:18 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Api Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register Api routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "api" middleware group. Enjoy building your Api!
 |
 */
 
@@ -19,35 +19,35 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // View Questions
-Route::middleware('auth:api')->get('/Question', 'API\QuestionController@index');
+Route::middleware('auth:api')->get('/Question', 'Api\QuestionController@index');
 
 // Store image
-Route::middleware('auth:api')->post('/Img', 'API\QuestionController@storeimg');
+Route::middleware('auth:api')->post('/Img', 'Api\QuestionController@storeimg');
 
 // Store Question
-Route::middleware('auth:api')->post('/Question', 'API\QuestionController@store');
+Route::middleware('auth:api')->post('/Question', 'Api\QuestionController@store');
 
 // Edit Question View
-Route::middleware('auth:api')->get('/Question{id}','API\QuestionController@edit');
+Route::middleware('auth:api')->get('/Question{id}','Api\QuestionController@edit');
 
 // Update Question
-Route::middleware('auth:api')->put('/Question/{id}','API\QuestionController@update');
+Route::middleware('auth:api')->put('/Question/{id}','Api\QuestionController@update');
 
 // Show Question
-Route::middleware('auth:api')->get('/Questions{id}', 'API\QuestionController@show');
+Route::middleware('auth:api')->get('/Questions{id}', 'Api\QuestionController@show');
 
 //Delete Question
-Route::middleware('auth:api')->delete('/Question/{id}', 'API\QuestionController@destroy');
+Route::middleware('auth:api')->delete('/Question/{id}', 'Api\QuestionController@destroy');
 
 // Store Answer
-Route::middleware('auth:api')->post('/Answer', 'API\AnswerController@store');
+Route::middleware('auth:api')->post('/Answer', 'Api\AnswerController@store');
 
 // Update Answer
-Route::middleware('auth:api')->put('/Answer/{id}','API\AnswerController@update');
+Route::middleware('auth:api')->put('/Answer/{id}','Api\AnswerController@update');
 
 //Delete Answer
-Route::middleware('auth:api')->delete('/Answer/{id}', 'API\AnswerController@destroy');
+Route::middleware('auth:api')->delete('/Answer/{id}', 'Api\AnswerController@destroy');
 
 // Store image
-Route::middleware('auth:api')->post('/Img', 'API\AnswerController@storeimg');
+Route::middleware('auth:api')->post('/Img', 'Api\AnswerController@storeimg');
 
